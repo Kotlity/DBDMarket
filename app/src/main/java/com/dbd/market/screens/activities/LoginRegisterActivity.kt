c010b72914d431d748973abbb20216baa5a9cfed
Hopeless Future <frankierafie12@gmail.com>
2022-10-22 22:45:40 +0300

    Added Introduction fragment

--- app/src/main/java/com/dbd/market/screens/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/dbd/market/screens/activities/LoginRegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.dbd.market
+package com.dbd.market.screens.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dbd.market.R
 
-class MainActivity : AppCompatActivity() {
+class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_register)
     }
 }
---
+++ app/src/main/java/com/dbd/market/screens/activities/MarketActivity.kt
@@ -0,0 +1,12 @@
+package com.dbd.market.screens.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dbd.market.R
+
+class MarketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_market)
+    }
+}
