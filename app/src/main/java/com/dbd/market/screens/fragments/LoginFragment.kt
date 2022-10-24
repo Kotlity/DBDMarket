390fb5e9c9c9807b56b0c6a5e8ce9182dc186b74
Hopeless Future <frankierafie12@gmail.com>
2022-10-25 00:17:37 +0300

    Added Login fragment

--- app/src/main/java/com/dbd/market/screens/fragments/LoginFragment.kt
+++ app/src/main/java/com/dbd/market/screens/fragments/LoginFragment.kt
@@ -1,23 +1,40 @@
 package com.dbd.market.screens.fragments
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dbd.market.R
+import com.dbd.market.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUnderlineToLinksTextViews()
+    }
+
+    private fun setUnderlineToLinksTextViews() {
+        val forgotPasswordCharSequence = getString(R.string.forgotPasswordString)
+        val registerCharSequence = getString(R.string.registerString)
+        val forgotPasswordSpannableString = SpannableString(forgotPasswordCharSequence)
+        val registerSpannableString = SpannableString(registerCharSequence)
+        forgotPasswordSpannableString.setSpan(UnderlineSpan(), resources.getInteger(R.integer.zero), forgotPasswordCharSequence.length, resources.getInteger(R.integer.zero))
+        registerSpannableString.setSpan(UnderlineSpan(), resources.getInteger(R.integer.zero), registerCharSequence.length, resources.getInteger(R.integer.zero))
+        binding.forgotPasswordLinkTextView.text = forgotPasswordSpannableString
+        binding.registerLinkTextView.text = registerSpannableString
     }
 
 }
