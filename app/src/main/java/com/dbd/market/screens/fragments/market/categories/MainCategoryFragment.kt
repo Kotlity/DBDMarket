380c3356016135b7ccf30b60143c993d73170d01
Hopeless Future <frankierafie12@gmail.com>
2022-11-04 21:21:42 +0200

    Created layout xml file for Main category fragment

--- app/src/main/java/com/dbd/market/screens/fragments/market/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/dbd/market/screens/fragments/market/categories/MainCategoryFragment.kt
@@ -1,23 +1,31 @@
 package com.dbd.market.screens.fragments.market.categories
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dbd.market.R
+import com.dbd.market.databinding.FragmentMainCategoryBinding
 
 class MainCategoryFragment : Fragment() {
+    private lateinit var binding: FragmentMainCategoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_category, container, false)
+        binding = FragmentMainCategoryBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupProgressBarColor()
+    }
+
+    private fun setupProgressBarColor() {
+        binding.mainCategoryProgressBar.progressTintList = ColorStateList.valueOf(Color.RED)
     }
 }
