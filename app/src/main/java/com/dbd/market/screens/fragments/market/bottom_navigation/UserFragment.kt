2aaae7841a6aeedc46ccda067f334005fcbf0133
Hopeless Future <frankierafie12@gmail.com>
2022-12-26 21:28:04 +0200

    Added AppBar and CollapsingToolbar to UserFragment layout

--- app/src/main/java/com/dbd/market/screens/fragments/market/bottom_navigation/UserFragment.kt
+++ app/src/main/java/com/dbd/market/screens/fragments/market/bottom_navigation/UserFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.dbd.market.databinding.FragmentUserBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class UserFragment : Fragment() {
     private lateinit var binding: FragmentUserBinding
@@ -20,5 +22,19 @@ class UserFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        makeFloatingActionButtonVisibleWhileCollapsingToolbar()
     }
+
+    private fun makeFloatingActionButtonVisibleWhileCollapsingToolbar() {
+        val layoutParams = binding.userTakePictureFloatingActionButton.layoutParams as CoordinatorLayout.LayoutParams
+        var fabBehavior = layoutParams.behavior as? FloatingActionButton.Behavior
+        fabBehavior?.let { floatingActionButtonBehavior ->
+            floatingActionButtonBehavior.isAutoHideEnabled = false
+        } ?: run {
+            fabBehavior = FloatingActionButton.Behavior()
+            fabBehavior?.isAutoHideEnabled = false
+            layoutParams.behavior = fabBehavior
+        }
+    }
+
 }
